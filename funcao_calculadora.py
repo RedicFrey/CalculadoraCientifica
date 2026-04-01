2d74e65f9f4449cee9a4ad4576649a544ac89c78
RedicFrey <danielcorradilavarini@gmail.com>
2026-04-01 09:41:54 -0300

    Add conversion option to calculator function

--- funcao_calculadora.py
+++ funcao_calculadora.py
@@ -4,6 +4,8 @@ import potencia as p
 import raizquadrada as rq
 import soma as sm
 import subtrai as s
+import Conversao as c
+
 
 # def perguntarnumeros():
 #    d = int(input("Qual o numero"))
@@ -11,32 +13,47 @@ import subtrai as s
     
 
 def calculadora():
-    print("\n\nqual operação deseja fazer?\n1. Soma\n2. subtração\n3. multplicação\n4. divisão\n5. potencia\n6. raiz quadrada")
+    print("\nQual operação deseja fazer?\n1. Soma\n2. subtração\n3. multplicação\n4. divisão\n5. potencia\n6. raiz quadrada\n7. Doliarrrrarrairr ain")
     print("\n")
-    decisao = int(input("escreva\n"))
-    
-    if(decisao == 1):
-        a = int(input("Qual o numero 1"))
-        b = int(input("Qual o numero 2"))
-        x = sm.somar(a, b)  
-    elif(decisao == 2):
-        b = int(input("Qual o numero 2\n"))
-        x = s.sub(a. b)
-    elif (decisao == 3):
-        a = int(input("Qual o numero 1\n"))
-        b = int(input("Qual o numero 2\n"))
-        x = m.mult(a, b) 
-    elif (decisao == 4):
-        a = int(input("Qual o numero 1\n"))
-        b = int(input("Qual o numero 2\n"))
-        x = d.divi(a, b)
-    elif (decisao == 5):
-        a = int(input("Qual o numero 1\n"))
-        b = int(input("Qual o numero 2\n"))  
-        x = p.poten(a, b)
-    elif(decisao == 6):
-        a = int(input("Qual o numero 1\n"))
-        x = rq.raiz(a)
+    while(True):
+        decisao = int(input("escreva\n"))
+        
+        if(decisao == 1):
+            a = int(input("Qual o numero 1\n"))
+            b = int(input("Qual o numero 2\n"))
+            x = sm.somar(a, b)  
+            break
+        elif(decisao == 2):
+            a = int(input("Qual o numero 1\n")) 
+            b = int(input("Qual o numero 2\n"))
+            x = s.sub(a, b)
+            break
+        elif (decisao == 3):
+            a = int(input("Qual o numero 1\n"))
+            b = int(input("Qual o numero 2\n"))
+            x = m.mult(a, b) 
+            break
+        elif (decisao == 4):
+            a = int(input("Qual o numero 1\n"))
+            b = int(input("Qual o numero 2\n"))
+            x = d.divi(a, b)
+            break
+        elif (decisao == 5):
+            a = int(input("Qual o numero 1\n"))
+            b = int(input("Qual o numero 2\n"))  
+            x = p.poten(a, b)
+            break
+        elif(decisao == 6):
+            a = int(input("Qual o numero 1\n"))
+            x = rq.raiz(a)
+            break
+        elif(decisao == 7):
+            x = c.Conversao() 
+            break
+        else:
+            print("opção invalida\n")
+            continue       
     return (x)    
+
 
 
