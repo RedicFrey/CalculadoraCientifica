bebf68cfb6899bdcb65c747a96623c9d25be8bb5
RedicFrey <danielcorradilavarini@gmail.com>
2026-04-01 09:41:12 -0300

    Implement user prompt for repeated calculations
    Added a loop to allow repeated calculations until the user decides to exit.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import funcao_calculadora as fq
 
-
 x = fq.calculadora()
 print(x)
+
+while True:
+    a = input("deseja fazer mais alguma coisa ?\n1. sim\n2. nao\n").lower().strip()
+    if a == "1" or a == "sim":
+        x = fq.calculadora()
+        print(x)
+    else:
+        print("Saindo...")
+        break
